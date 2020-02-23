8cc4d75b839a0e354a416980d9886b4601231613
arthurarty <arthur.nangai@gmail.com>
2020-02-23 16:49:21 +0300

    ft(create-drink) - Ensure only manage can create.

--- backend/src/api.py
+++ backend/src/api.py
@@ -46,16 +46,8 @@ def get_drinks_detail():
     })
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
 def create_drink():
     body = request.get_json()
     validated_body = {}
@@ -73,7 +65,7 @@ def create_drink():
         new_drink.insert()
         return jsonify({
             'success': True,
-            'created': new_drink.id,
+            'drinks': new_drink.long(),
         }), 201
     except DatabaseError:
         abort(422)
